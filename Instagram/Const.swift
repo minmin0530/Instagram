3dd10aa64674e47d3d00a26f5872cf9ebcd80768
minmin0530 <izumiyoshiki@icloud.com>
2019-02-10 14:39:35 +0900

    9.4 タイムライン表示完成

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by IzumiYoshiki on 2019/02/10.
+//  Copyright © 2019 Yoshiki Izumi. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let PostPath = "posts"
+}
